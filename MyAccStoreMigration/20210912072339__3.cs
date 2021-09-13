c2a3ab50d15e57fa221062082c16ee67fe8066d9
esmaeel motaghi <esmaeel.m@live.com>
2021-09-13 12:44:07 +0430

    idk

---
+++ MyAccStoreMigration/20210912072339__3.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Store.MyAccStoreMigration
+{
+    public partial class _3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PinCode",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PinCode",
+                table: "AspNetUsers");
+        }
+    }
+}
