4c09914676fdbf2dbced2597c1c5bbc64abaa1f9
esmaeel motaghi <esmaeel.m@live.com>
2021-09-13 20:53:28 +0430

    try to do somthing

--- ClientApp/src/app/front/home/home.component.ts
+++ ClientApp/src/app/front/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { HttpService } from '../http.service';
 import { OwlOptions } from 'ngx-owl-carousel-o';
+import { toTreeHelper } from '../../../../../../Santel/Core/ClientApp/src/app/services/utils';
 
 @Component({
   selector: 'app-home',
@@ -12,7 +13,12 @@ export class HomeComponent implements OnInit {
 
   constructor(public http: HttpService) { }
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
+    let data :any= await this.http.get('​api​/Front​/GetInitial').toPromise(); 
+    console.log(data);
+    let x = toTreeHelper(data.categories, 'Id', 'ParentId', null);
+    console.log(x);
+
   }
   customOptions: OwlOptions = {
     dots: true,
@@ -21,7 +27,7 @@ export class HomeComponent implements OnInit {
     autoWidth:true,
     margin:10,
     navSpeed: 700,
-    navText: ['<', '>'],
+    navText: ['>', '<'],
     rtl: true,
     nav:true,
     responsive: {
@@ -30,4 +36,31 @@ export class HomeComponent implements OnInit {
       },
     }
   }
+
+  customOptions3: OwlOptions = {
+    dots: false,
+    loop: true,
+    center: true,
+    autoWidth: true,
+    margin: 10,
+    navSpeed: 700,
+    navText: ['بعدی', 'قبلی'],
+    rtl: true,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      500: {
+        items: 3
+      },
+      1000: {
+        items: 4
+      },
+      2000: {
+        items: 5
+      },
+    }
+  }
+
 }
--- FrontController.cs
+++ FrontController.cs
@@ -1,11 +1,26 @@
 ﻿using Core.DB;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Store.Models
 {
+    [Route("api/[controller]/[action]")]
     public class FrontController:Controller
     {
         public StoreDB _context { get; }
-        public FrontController() { }
+        public FrontController(StoreDB dB)
+        {
+            this._context = dB;
+        }
+
+
+        [HttpGet]
+        public async Task<JsonResult> GetInitial()
+        {
+            var items =await _context.Categories.Where(c => c.Status == Core.Models.Statuses.Published).ToListAsync();
+            return new JsonResult(new { categories = items });
+        }
     }
 }
